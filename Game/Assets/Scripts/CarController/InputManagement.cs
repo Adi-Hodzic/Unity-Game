0eedf4ad58fa6dfe26dad4ea7df8e33bebf865b2
Adi993 <hodzic.adi@edu.fit.ba>
2021-03-18 22:11:01 +0100

    Game

---
+++ Game/Assets/Scripts/CarController/InputManagement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Car.CarController
+{
+    public class InputManagement : MonoBehaviour
+    {
+        public Action<float> OnInputChanged;
+
+        private void Update()
+        {
+            OnInputChanged?.Invoke(Input.GetAxis("Horizontal"));
+        }
+    }
+}
---
+++ Game/Assets/Scripts/CarController/RearContr.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RearContr : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0f, 0, 0);
+    }
+}
