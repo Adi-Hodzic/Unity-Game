a0822492476d1d72dd3ca7215210d9a52741859a
Adi993 <hodzic.adi@edu.fit.ba>
2021-04-08 14:43:03 +0200

    Update

--- Game/Assets/Scripts/CarController/CarController.cs
+++ Game/Assets/Scripts/CarController/CarController.cs
@@ -8,71 +8,61 @@ namespace Car.CarController
         [SerializeField] [Range(0.001f, 100)] private float Speed;
         [SerializeField] [Range(0.001f, 100)] private float TurnSpeed;
         [SerializeField] private InputManagement.InputManagement Manager;
-        //[SerializeField] public WheelCollider[] wheelCollider = new WheelCollider[4];
-        //[SerializeField] public Transform[] wheelMesh = new Transform[4];
-        [SerializeField ]public Transform t_CenterOfMass;
+        [SerializeField] private Transform CenterOfMass;
+        [SerializeField] private LayerMask GroundLayer;
 
-        private Rigidbody r_Ridgedbody;
-        //public Transform FrontTarget;
-        //public Transform BackTarget;
-
+        private Rigidbody Rigidbody;
+        private bool IsCarGrounded;
         void Start()
         {
             Manager.OnInputChanged += OnInputChanged;
-
-            r_Ridgedbody = GetComponent<Rigidbody>();
-            r_Ridgedbody.centerOfMass = t_CenterOfMass.localPosition;
+            this.transform.position = Rigidbody.position;
+            Rigidbody = GetComponent<Rigidbody>();
+            Rigidbody.centerOfMass = CenterOfMass.localPosition;
         }
 
         private void OnInputChanged(float Horizontal, float Vertical)
         {
-            //Forward and back
-            if (Vertical > 0)
-                this.transform.Translate(Vector3.forward * Time.deltaTime * Speed * Vertical);
-            else if (Vertical < 0)
-                this.transform.Translate(Vector3.forward * Time.deltaTime * 10 * Vertical);
 
+            //Forward and back
+            RaycastHit Hit;
+            IsCarGrounded = Physics.Raycast(transform.position, -transform.up, out Hit, 1f, GroundLayer);
+            if (!IsCarGrounded)
+            {
+                if (Vertical > 0)
+                    this.transform.Translate(Vector3.forward * Time.deltaTime * Speed * Vertical);
+                else if (Vertical < 0)
+                    this.transform.Translate(Vector3.forward * Time.deltaTime * 10 * Vertical);
+            }
             //Right and left
             if (Horizontal != 0 && Vertical > 0)
                 this.transform.Rotate(Vector3.up, TurnSpeed * Time.deltaTime * Horizontal);
             else if (Vertical < 0 && Horizontal != 0)
                 this.transform.Rotate(Vector3.up, TurnSpeed * Time.deltaTime * -Horizontal);
+
+        }
+        private void FixedUpdate()
+        {
+
+
+
+
+
+
+
         }
 
+        //////////private void FixedUpdate()
+        //////////{
+        //////////    if (isCarGrounded)
+        //////////        Sphere.AddForce(transform.forward * MoveInput, ForceMode.Acceleration);
+        //////////    else
+        //////////        Sphere.AddForce(transform.up * -20f);
+        //////////}
 
-        //public void Update()
-        //{
-        //    UpdateMeshPosition();
-        //}
+        //public Transform FrontTarget;
+        //public Transform BackTarget;
 
-        //public void FixedUpdate()
-        //{
-        //    float steer = Input.GetAxis("Horizontal") * maxSteerAngle;
-        //    float torque = Input.GetAxis("Vertical") * maxTorque;
 
-        //    wheelCollider[0].steerAngle = steer;
-        //    wheelCollider[1].steerAngle = steer;
-
-        //    for (int i = 0; i < 4; i++)
-        //    {
-        //        wheelCollider[i].motorTorque = torque;
-        //    }
-        //}
-
-        //public void UpdateMeshPosition()
-        //{
-        //    for (int i = 0; i < 4; i++)
-        //    {
-        //        Quaternion quat;
-        //        Vector3 pos;
-
-        //        //Gets the current position of the physics WheelColliders.
-        //        wheelCollider[i].GetWorldPose(out pos, out quat);
-
-        //        ///Sets the mesh to match the position and rotation of the physics WheelColliders.
-        //        wheelMesh[i].position = pos;
-        //        wheelMesh[i].rotation = quat;
-        //    }
-        //}
     }
 }
