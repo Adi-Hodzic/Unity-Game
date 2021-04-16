0795153fefe06187d882ea3f5ea357883e28d550
Adi993 <hodzic.adi@edu.fit.ba>
2021-04-16 18:30:27 +0200

    CarController

--- Game/Assets/Scripts/CarController/CarController.cs
+++ Game/Assets/Scripts/CarController/CarController.cs
@@ -5,32 +5,31 @@ namespace Car.CarController
 {
     public class CarController : MonoBehaviour
     {
-        [ReorderableList] [SerializeField] private List<GameObject> Wheels;
-        [SerializeField] private InputManagement.InputManagement Manager;
-        [SerializeField] private Transform CenterOfMass;
-        [SerializeField] private LayerMask GroundLayer;
+        [SerializeField] [ReorderableList] private List<GameObject> Wheels;
         [SerializeField] [Range(0.001f, 100)] private float MaxSpeedT;
         [SerializeField] [Range(0.001f, 20)] private float MaxSpeedR;
         [SerializeField] [Range(0.001f, 100)] private float TurnSpeed;
+        [SerializeField] private Transform CenterOfMass;
+        [SerializeField] private LayerMask GroundLayer;
+        [SerializeField] private InputManagement.InputManagement Manager;
         [SerializeField] private float MaximumRotation;
         [SerializeField] private float RotationSpeed;
 
         private float CurrentRotation { get; set; }
         private float CurrentSpeed { get; set; }
         private float Last { get; set; }
-        //private Rigidbody Rigidbody;
+        private Rigidbody Rigidbody;
         //private bool IsCarGrounded;
-
 
         void Start()
         {
             Manager.OnInputChanged += OnInputChanged;
             Manager.OnBrake += OnBrake;
-            //Rigidbody = GetComponent<Rigidbody>();
-            //Rigidbody.centerOfMass = CenterOfMass.localPosition;
-            //this.transform.position = Rigidbody.position;
+            Rigidbody = GetComponent<Rigidbody>();
+            Rigidbody.centerOfMass = CenterOfMass.localPosition;
+            this.transform.position = Rigidbody.position;
         }
-       
+
         private void OnBrake()
         {
             if (CurrentSpeed > 0) CurrentSpeed -= 1.5f;
@@ -38,13 +37,12 @@ namespace Car.CarController
         }
         private void OnInputChanged(float Horizontal, float Vertical)
         {
-            Debug.Log(CurrentSpeed);
             if (Vertical != 0)
             {
                 Last = Vertical;
             }
 
-            if (Vertical > 0 || Vertical < 0)  
+            if (Vertical > 0 || Vertical < 0)
             {
                 //Forward
                 if (Vertical > 0)
@@ -52,18 +50,15 @@ namespace Car.CarController
                     if (CurrentSpeed < MaxSpeedT)
                         CurrentSpeed += 1;
                     this.transform.Translate(Vector3.forward * Time.deltaTime * CurrentSpeed * Vertical);
-                    foreach (GameObject i in Wheels)
-                        i.transform.Rotate(CurrentSpeed, i.transform.localRotation.y, i.transform.localRotation.z);
                 }
-               
                 //Back
-                else if (Vertical < 0 && CurrentSpeed<MaxSpeedR)
+                else if (Vertical < 0 && CurrentSpeed < MaxSpeedR)
                 {
                     if (CurrentSpeed < MaxSpeedR)
                         CurrentSpeed += 0.5f;
                     this.transform.Translate(Vector3.forward * Time.deltaTime * CurrentSpeed * Vertical);
-                    foreach (GameObject i in Wheels)
-                        i.transform.Rotate(-CurrentSpeed, i.transform.localRotation.y, i.transform.localRotation.z);
+                    //foreach (GameObject i in Wheels)
+                    //    i.transform.Rotate(-CurrentSpeed, i.transform.localRotation.y, i.transform.localRotation.z);
                 }
                 //ControllingSpeedWhenUWantToGoBack
                 else if (CurrentSpeed > MaxSpeedR && Vertical < 0)
@@ -83,12 +78,6 @@ namespace Car.CarController
                 else if (Vertical < 0 && Horizontal != 0)
                 {
                     this.transform.Rotate(Vector3.up, TurnSpeed * Time.deltaTime * -Horizontal);
-                }
-                //Speed-wheels
-                else
-                {
-                    foreach (GameObject i in Wheels)
-                        i.transform.Rotate(CurrentSpeed, i.transform.localRotation.y, i.transform.localRotation.z);
                 }
             }
             else
@@ -114,31 +103,18 @@ namespace Car.CarController
             CurrentRotation = Horizontal * RotationSpeed * Time.deltaTime;
             if (CurrentRotation <= MaximumRotation || CurrentRotation >= -MaximumRotation)
             {
-                Wheels[0].transform.localRotation = Quaternion.Euler(CurrentSpeed, CurrentRotation, Wheels[0].transform.localRotation.z);
-                Wheels[1].transform.localRotation = Quaternion.Euler(CurrentSpeed, CurrentRotation, Wheels[1].transform.localRotation.z);
+                Vector3 from_v = new Vector3(Wheels[0].transform.rotation.x, Wheels[0].transform.localRotation.y, Wheels[0].transform.localRotation.z);
+                Vector3 to_v = new Vector3(Wheels[1].transform.rotation.x, CurrentRotation, Wheels[1].transform.localRotation.z);
+                Quaternion from = Quaternion.Euler(from_v);
+                Quaternion to = Quaternion.Euler(to_v);
+                Wheels[0].transform.localRotation = Quaternion.Lerp(from, to, 1f);
+                Wheels[1].transform.localRotation = Quaternion.Lerp(from, to, 1f);
             }
+            //Speed-wheels
+            int NegOrPos = 1;
+            if (Last < 0) NegOrPos = -1;
+            foreach (GameObject i in Wheels)
+                i.transform.Rotate(NegOrPos * CurrentSpeed, i.transform.localRotation.y, i.transform.localRotation.z);
         }
-        //private void FixedUpdate()
-        //{
-        //if (CurrentSpeed != 0)
-        //{
-        //    CurrentSpeed -= 2;
-        //    this.transform.Translate(Vector3.forward * Time.deltaTime * CurrentSpeed * Vert);
-        //}
-        //}
-
-        //////////private void FixedUpdate()
-        //////////{
-        //////////    if (isCarGrounded)
-        //////////        Sphere.AddForce(transform.forward * MoveInput, ForceMode.Acceleration);
-        //////////    else
-        //////////        Sphere.AddForce(transform.up * -20f);
-        //////////}
-
-        //public Transform FrontTarget;
-        //public Transform BackTarget;
-
-        //RaycastHit Hit;
-        //IsCarGrounded = Physics.Raycast(transform.position, -transform.up, out Hit, 1f, GroundLayer);
     }
 }
---
+++ Game/Assets/Scripts/CarController/Nav.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace Player.CarController
+{
+    public class Nav : MonoBehaviour
+    {
+        private NavMeshAgent navAgent;
+        [SerializeField] private GameObject cars;
+        void Start()
+        {
+            navAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            navAgent.SetDestination(cars.transform.position);
+        }
+    }
+}
